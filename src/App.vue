e321155257c15fa846f11fe292555acf14bf8fc1
reyhardy <rezahadzri@gmail.com>
2018-09-15 16:46:55 +0800

    vue bulma

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,108 @@
 <template>
     <div class="container">
         <div class="columns is-mobile">
-            <div class="column">
-                <figure class="image is-128x128">
-                    <img src="./assets/bulma.png" alt="">
-                </figure>
-                <a class="button" href="https://bulma.io">Bulma.io</a>
+            <section class="column">
+                <div class="content">
+                    <a href="http://vuejs.org">
+                        <figure class="image">                    
+                            <img src="./assets/vuejs2.jpg" alt="" height="280px" >
+                        </figure>
+                    </a>
+                </div>
+            </section>
+            <section class="column">
+                <div class="content">
+                    <a href="https://bulma.io">
+                        <figure class="image">
+                            <img src="./assets/bulma2.png" alt="">
+                        </figure>
+                    </a>  
+                </div>
+            </section>
+        </div>
+        <section class="section">
+            <p class="subtitle is-2">template for vuejs with bulma</p>
+        </section>
+        <section class="section">
+            <p class="subtitle is-4">Download
+                <a href="">
+                    <i class="fab fa-github"></i>
+                </a>     
+            </p>
+        </section>
+        
+<!--         
+        <section class="section">
+            <div class="columns is-mobile">
+                <div class="column">
+                    <div class="box">
+                        <a href="http://vuejs.org">
+                            <figure class="image is-128x128">                    
+                                <img src="./assets/vuejs2.jpg" alt="" height="280px" >
+                            </figure>
+                        </a>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="box">
+                        <a href="">
+                            <figure class="image">
+                                <img src="./assets/bulma.png" alt="">
+                            </figure>
+                        </a>  
+                    </div>              
+                </div>
             </div>
-            <div class="column">
-                <figure class="image is-128x128">
-                    <img src="./assets/vuejs.png" alt="" >
+        </section>
+        <div class="columns">
+            <div class="column is-three-fifths is-offset-one-fifth">
+                <p>View source on github</p>
+                <figure class="image is-48x48">
+                    <img src="./assets/github.png" alt="">
                 </figure>
-                <a class="button" href="http://vuejs.org">Vue.js</a>
             </div>
         </div>
-        <!-- <div class="hero is-primary">
-            <div class="hero-body">
-                <h1 class="title is-1 is-primary">Primary</h1>
-                <h1 class="subtitle">Primary</h1>
-            </div>
-        </div>
-        <div class="hero is-info">
-            <div class="hero-body">
-                <h2 class="title is-2">Merah mu Bara</h2>
-                <h1 class="subtitle">Merah mu Bara</h1>
-            </div>
+        <div class="tile"></div>         -->
     </div>
 </template> 
 
 <script>
-
+export default{
+    name: "App"
+}
 </script>
 
 <style>
-.column {
-    margin : 5px auto 5px auto;
+section{
+    text-align: center;
 }
+/*
+.columns{
+    border: 1px solid;
+}
+.column{
+    border: 1px dashed red;
+    height: 350px;
+}
+
+section.columns.column{
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+section.columns.column > .box{
+    top: 50%;
+    position: absolute;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+}
+.image{
+    flex: 1;
+    
+}
+*/
 </style>
